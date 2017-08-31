93f51e18fde99f3038e4209c1fba586cd1cb7489
Levi Smith <levi.charles.smith@gmail.com>
2017-08-30 21:08:52 -0400

    message

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,1 +1,16 @@
 
+$(document).ready(function () {
+    $('h1.hidden').fadeIn(1000).removeClass('hidden');
+});
+
+setTimeout(function (){
+
+  $('h2.hidden').fadeIn(1000).removeClass('hidden');
+
+}, 1000);
+
+setTimeout(function (){
+
+  $('div.hidden').fadeIn(1000).removeClass('hidden');
+
+}, 2000);
